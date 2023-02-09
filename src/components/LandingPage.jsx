803cfa28eb54730773f873911ebb10e3c3d52e77
Kanishk Singh <kanishk.singh198@gmail.com>
2023-02-10 02:27:55 +0530

    initial commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const LandingPage = () => {
+	const [data, setData] = useState([]);
+
+	async function requestData() {
+		const res = await fetch("https://fakestoreapi.com/products");
+		const json = await res.json();
+
+		setData(json);
+	}
+
+	useEffect(() => {
+		//fetch data on load
+		requestData()
+			.then()
+			.catch((e) => console.error(e));
+	}, []);
+
+	return (
+		<>
+			<h1>Products</h1>
+			{data.map((product) => {
+				return <p>{product.title}</p>;
+			})}
+		</>
+	);
+};
+
+export default LandingPage;
